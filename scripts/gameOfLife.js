cc8a1e86698a38b59e206175b144f8b3346edb6c
JamesHarrisonZa <james.harrison.za@gmail.com>
2017-07-10 10:02:24 +0200

    initial commit

---
+++ scripts/gameOfLife.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var getNextGeneration = (cells) => {
+
+	var getCell = (cells, r, c) => { return (cells[r] && cells[r][c]) | 0; }
+
+	return cells.map((row, r) => {
+		return row.map((isAlive, c) => {
+
+			const neighbours =
+				getCell(cells, r - 1, c - 1) + getCell(cells, r - 1, c) + getCell(cells, r - 1, c + 1) +
+				getCell(cells, r, c - 1) 						        + getCell(cells, r, c + 1) +
+				getCell(cells, r + 1, c - 1) + getCell(cells, r + 1, c) + getCell(cells, r + 1, c + 1)
+
+			return (neighbours === 3 || (neighbours === 2 && isAlive)) | 0;
+		});
+	});
+}
---
+++ scripts/grid.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const _gridHeight = 10;
+const _gridWidth = 10;
+let _cells = getStartingCells();
+
+function initialize() {
+
+	const refreshSeconds = 1;
+	createEmptyDivs(_gridHeight, _gridWidth);
+	window.setInterval(updateGrid, refreshSeconds * 1000);
+}
+
+function createEmptyDivs(gridHeight, gridWidth) {
+
+	//Create row with x columns
+	for (let x = 0; x < gridWidth; x++) {
+		let colDiv = window.document.createElement('div');
+		window.document.querySelector('.row').appendChild(colDiv);
+	}
+
+	//duplicate y times. -1 because we started with 1 row
+	for (let y = 0; y < gridHeight - 1; y++) {
+		duplicateRow();
+	}
+}
+
+function getStartingCells(){
+
+	return [
+		[1,0,0,0,0,0,0,0,0,1],
+		[0,1,0,0,0,0,0,0,1,0],
+		[0,0,1,0,0,0,0,1,0,0],
+		[0,0,0,1,0,0,1,0,0,0],
+		[0,0,0,0,1,1,0,0,0,0],
+		[0,0,0,0,1,1,0,0,0,0],
+		[0,0,0,1,0,0,1,0,0,0],
+		[0,0,1,0,0,0,0,1,0,0],
+		[0,1,0,0,0,0,0,0,1,0],
+		[1,0,0,0,0,0,0,0,0,1]
+	];
+
+}
+
+function duplicateRow() {
+	let allRows = document.querySelectorAll('.row');
+	let lastRow = allRows[allRows.length - 1];
+	let clone = lastRow.cloneNode(true);
+	document
+		.querySelector('.grid')
+		.appendChild(clone);
+}
+
+function updateGrid() {
+
+	_cells = getNextGeneration(_cells);
+
+	let allRows = document.querySelectorAll('.row');
+
+	for(let y = 0; y < _gridHeight; y++){
+
+		var rowDiv = allRows[y];
+
+		for(let x = 0; x < _gridWidth; x++){
+
+			let colDiv = rowDiv.childNodes[x];
+			let cell = _cells[y][x];
+
+			setIsActive(colDiv, cell); //randomBinary() Change to pass in 1 or 0
+		}
+	}
+}
+
+function setIsActive(cellDiv, isActive) {
+	if (isActive) {
+		cellDiv.classList.remove('inactive')
+		cellDiv.classList.add('active')
+	} else {
+		cellDiv.classList.remove('active')
+		cellDiv.classList.add('inactive')
+	}
+}
+
+//ToDo: Take out after playing
+function randomBinary() {
+	let max = 1
+	let min = 0
+	return Math.floor(
+		Math.random() * (max - min + 1)
+	)
+}
+
+initialize();
