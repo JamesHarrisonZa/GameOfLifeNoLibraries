9f129e9ccdca674a5b284832c1b4301f73198cd6
JamesHarrisonZa <james.harrison.za@gmail.com>
2017-07-11 14:13:42 +0200

    Tweaked _fillPercentage and animation duration

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,5 @@
 const _refreshSeconds = 0.2;
-const _fillPercentage = 25;
+const _fillPercentage = 40; //Disable animations if you want to fill more
 const _gridHeight = getGridHeight();
 const _gridWidth = getGridWidth();
 let _cells = getStartingCells();
